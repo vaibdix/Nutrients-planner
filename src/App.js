6a4753ce4ed4ff70a100ba59a407ded6e54246d8
Vaibhav Dixit <43854458+vaibdix@users.noreply.github.com>
2021-01-21 14:24:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import MealList from "./MealList";
+
+function App() {
+  const [mealData, setMealData] = useState(null);
+  const [calories, setCalories] = useState(2000);
+
+  function getMealData() {
+    fetch(
+      //add api here
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setMealData(data);
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  }
+
+  function handleChange(e) {
+    setCalories(e.target.value);
+  }
+
+  return (
+    <div className="App">
+      <section className="controls">
+        <input
+          type="number"
+          placeholder="Calories (e.g. 2000)"
+          onChange={handleChange}
+        />
+        <button onClick={getMealData}>Get Daily Meal Plan</button>
+      </section>
+      {mealData && <MealList mealData={mealData} />}
+    </div>
+  );
+}
+
+export default App;
